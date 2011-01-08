d1a3b9370fed7651e54bcfea8c38ca1ad0705550
Ronnie <unlights@gmail.com>
2011-01-09 00:13:27 +0300

    First commit

---
+++ elrte.functions.js
@@ -0,0 +1,127 @@
+var RtePlugin = {
+	
+	bElRteLoaded: false,
+	
+	AddCssFile: function(file, webroot) {
+		var link, filepath = '';
+		link = $('<link>', {rel: 'stylesheet', type: 'text/css', media: 'screen', charset:'utf-8'});
+		if (webroot) filepath = webroot + '/';
+		filepath += file;
+		filepath = filepath.replace(/\/\//, '/');
+		$(link).attr('href', filepath);
+		$('head').append(link);
+	},
+	
+	AddJsFile: function(file, webroot) {
+		var script = $('<script>', {type: 'text/javascript', charset:'utf-8'});
+		var filepath = '';
+		if (webroot) filepath = webroot + '/';
+		filepath += file;
+		filepath = filepath.replace(/\/\//, '/');
+		$(script).attr('src', filepath);
+		$('head').append(script);
+	},
+	
+	LoadElRte: function() {
+		var self = RtePlugin;
+		if (self.bElRteLoaded) return;
+		self.bElRteLoaded = true;
+		lang = gdn.definition('LocaleLanguageCode', 'en');
+		
+		var rteroot = gdn.definition('ElRteVendorWebRoot');
+		self.AddCssFile('css/smoothness/jquery-ui-1.8.7.custom.css', rteroot);
+		self.AddCssFile('css/elrte.min.css', rteroot);
+		self.AddJsFile('js/jquery-ui-1.8.7.custom.min.js', rteroot);
+		self.AddJsFile('js/elrte.min.js', rteroot);
+		self.AddJsFile('js/i18n/elrte.'+lang+'.js', rteroot);
+		
+		var elfinderwebroot = gdn.definition('ElFinderVendorWebRoot', false);
+		if (elfinderwebroot != false) {
+			self.AddCssFile('js/ui-themes/base/ui.all.css', elfinderwebroot);
+			self.AddCssFile('css/elfinder.css', elfinderwebroot);
+			self.AddJsFile('js/elfinder.min.js', elfinderwebroot);
+			self.AddJsFile('js/i18n/elfinder.'+lang+'.js', elfinderwebroot);			
+		}
+	},
+	
+	AddChunkButtonsToTextarea: function() {
+		var self = RtePlugin;
+		var Wrap = $('<span>', {class:'RteWrap'});
+		$("textarea").each(function(index, element){
+			var Id = $(element).attr('id');
+			var label = $('label[for='+Id+']', $(this).parent());
+			// 1. textarea has label
+			if (label.length > 0) {
+				label = label.first();
+				var button = $('<span>', {class:'RteButtonC', html:'Ω'});
+				button.bind('click', self.ClickToggleHandler);
+				label.append(button);
+				return;
+			}
+			// 2. textarea without label
+			$(element).wrap(Wrap);
+			$(element).after($('<span>', {html:'Ω', click: self.ClickToggleHandler}));
+		});
+	},
+	
+	AddCustomPanels: function() {
+		var p = elRTE.prototype.options.panels;
+		p.CopyPaste = ['pastetext', 'pasteformattext', 'removeformat', 'docstructure'];
+		p.TextDecoration = ['bold', 'italic', 'underline'];
+		p.TextAlign = ['justifyleft', 'justifycenter', 'justifyright'];
+		p.FormatHeaders = ['formatblock'];
+		p.List = ['insertorderedlist', 'insertunorderedlist'];
+		p.Renders = ['link', 'unlink', 'image']; // anchor
+		p.Misc = ['elfinder', 'fullscreen'];
+	},
+	
+	ClickToggleHandler: function() {
+		var self = RtePlugin;
+		if (!self.bElRteLoaded) self.LoadElRte();
+		self.AddCustomPanels();
+		elRTE.prototype.options.toolbars.RtePluginToolbar = [
+			'CopyPaste', 'undoredo', 'TextDecoration', 'TextAlign', 'FormatHeaders', 'List', 'Renders', 'Misc'
+		];
+		
+		var lang = gdn.definition('LocaleLanguageCode', 'en');
+		var elfinder = gdn.definition('ElFinderVendorWebRoot', false);
+		elfinder = !!elfinder;
+		
+		var options = {
+			doctype: '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">',
+			absoluteURLs: false,
+			allowSource: true,
+			styleWithCSS: false,
+			fmAllow: elfinder,
+			toolbar: 'RtePluginToolbar',
+			height:400,
+			lang: lang,
+			fmOpen : function(callback) {
+				$('<div id="myelfinder" />').elfinder({
+					url: gdn.url('/plugin/elfinderconnector'),
+					lang: lang,
+					dialog: {width: '95%', modal: true, title: ''},
+					closeOnEditorCallback: true,
+					editorCallback: callback
+				});
+			}
+		};
+		var pp = $(this).parent().parent();
+		//console.log(this, pp);
+		var t = pp.find('textarea').first();
+		options.height = $(t).height();
+		
+		var rte = t.is(':hidden');
+		
+		if (!rte) {
+			t.elrte(options);
+		} else { // TODO: FIX ME, CANT RE-ENABLE WYSIWYG
+			t.elrte('updateSource');
+			$(pp).find('div.el-rte').replaceWith(t);
+			t.show();
+		}
+	}
+}
+
+
+jQuery(document).ready(RtePlugin.AddChunkButtonsToTextarea);
---
+++ elrte.plugin.php
@@ -0,0 +1,98 @@
+<?php if (!defined('APPLICATION')) exit();
+
+$PluginInfo['elRTE'] = array(
+	'Name' => 'elRTE',
+	'Description' => 'elRTE is an open-source WYSIWYG HTML-editor written in JavaScript using jQuery UI.',
+	'Version' => '1.0.0',
+	'Date' => '8 Jan 2011',
+	'Author' => 'Hrusha',
+	// TODO: Group permissions
+	'RegisterPermissions' => array(
+		'Plugins.ElRte.Wysiwyg.Allow',
+		'Plugins.ElRte.FileManager.Allow',
+		'Plugins.ElRte.FileManager.Root',
+		//'Plugins.ElRte.FileManager.Group',
+		'Plugins.ElRte.FileManager.Files.Read',
+		'Plugins.ElRte.FileManager.Files.Write',
+		'Plugins.ElRte.FileManager.Files.Remove'
+	)
+);
+
+class ElRtePlugin extends Gdn_Plugin {
+	
+/*	public function __construct() {
+	}*/
+	
+	protected static function LocaleLanguageCode() {
+		$T = preg_split('/[_-]/', Gdn::Locale()->Current());
+		return ArrayValue(0, $T, 'en');
+	}
+	
+	public function PluginController_ElFinderConnector_Create($Sender) {
+		require_once dirname(__FILE__).'/class.filemanager.php';
+		$Options = self::GetOptions();
+		$FileManager = new ElFinderFileManager($Options);
+		$FileManager->Run();
+	}
+	
+	protected static function GetOptions() {
+		
+		$Options = array();
+		$Defaults = array('read'  => True, 'write' => False, 'rm' => False);
+		$All = array('read'  => True, 'write' => True, 'rm' => True);
+		$DefaultOptions = array(
+			'root' => Null,
+			'tmbDir' => '.thumbnails',
+			'defaults' => $Defaults,
+			'perms' => array()
+		);
+		$DS = DIRECTORY_SEPARATOR;
+		$NowYear = date('Y');
+		$CustomPermissions = array(
+			"~$NowYear{$DS}.*~" => $All
+		);
+		
+		$Session = Gdn::Session();
+	
+		if ($Session->CheckPermission('Plugins.ElRte.Wysiwyg.Allow')) {
+			$Options['defaults'] = $All;
+			
+			if ($Session->CheckPermission('Plugins.ElRte.FileManager.Root')) {
+				$Options['root'] = 'uploads';
+				$Options['URL'] = Url('uploads'). '/';
+			} elseif ($Session->CheckPermission('Plugins.ElRte.FileManager.Allow')) {
+				$UserID = GetValueR('User.UserID', $Session, 0);
+				if (!$UserID || $UserID <= 0) throw new Exception('UserID is none.');
+				$Folder = 'uploads/userfiles/'.$UserID;
+				if (!is_dir($Folder)) mkdir($Folder);
+				$Options['root'] = $Folder;
+				$Options['URL'] = Url($Folder). '/';
+			}
+		}
+		$Options = array_merge($DefaultOptions, $Options);
+		return $Options;
+	}
+	
+	public function Base_Render_Before($Sender) {
+		if (!($Sender->DeliveryType() == DELIVERY_TYPE_ALL 
+			&& $Sender->SyndicationMethod == SYNDICATION_NONE)) return;
+		
+		$Session = Gdn::Session();
+		if ($Session->CheckPermission('Plugins.ElRte.Wysiwyg.Allow')) {
+			$Sender->AddDefinition('LocaleLanguageCode', self::LocaleLanguageCode());
+			$Sender->AddDefinition('ElRteVendorWebRoot', $this->GetWebResource('elrte-1.2'));
+			
+			if ($Session->CheckPermission('Plugins.ElRte.FileManager.Allow')) {
+				$Sender->AddDefinition('ElFinderVendorWebRoot', $this->GetWebResource('elfinder-1.1'));
+			}
+			
+			$Sender->AddJsFile($this->GetWebResource('elrte.functions.js'));
+			$Sender->AddCssFile($this->GetWebResource('design/elrte.plugin.css'));
+			
+		}
+
+	}
+	
+	public function Setup() {
+	}
+}
